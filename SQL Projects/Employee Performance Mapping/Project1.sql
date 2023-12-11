a8a40312e6398bf60639e223c86ecf86f5450770
Razan Alhasan <alhasan.razan1@gmail.com>
2023-12-12 02:24:45 +0300

    Employee_Performance.sql

--- SQL Projects/Employee Performance Mapping/Project1.sql
+++ SQL Projects/Employee Performance Mapping/Project1.sql
@@ -27,8 +27,8 @@ FROM emp_record_table
 WHERE EMP_RATING < 2 OR EMP_RATING > 4 OR EMP_RATING BETWEEN 2 AND 4;
 
 /*
-4- Fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the 
-employee record table, and make a list of employees and details of their department.
+4- Concatenate the FIRST_NAME and the LAST_NAME of employees in the Finance 
+department from the employee table and then give the resultant column alias as NAME.
     */
 
 SELECT CONCAT(FIRST_NAME, " " ,LAST_NAME) AS FullName
@@ -36,8 +36,8 @@ FROM emp_record_table
 WHERE DEPT = "Finance";
 
 /*
-5- Fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the 
-employee record table, and make a list of employees and details of their department.
+5- List only those employees who have someone reporting to them. Also, show the 
+number of reporters (including the President).
     */
 
 SELECT e.EMP_ID, CONCAT(e.FIRST_NAME, " ", e.LAST_NAME) AS EmployeeName,
@@ -47,8 +47,8 @@ WHERE e.EMP_ID IN (SELECT DISTINCT MANAGER_ID FROM emp_record_table)
 ORDER BY e.EMP_ID;
 
 /*
-6- Fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the 
-employee record table, and make a list of employees and details of their department.
+6- List down all the employees from the healthcare and finance departments using 
+union. Take data from the employee record table.
     */
 
 SELECT EMP_ID, FIRST_NAME, LAST_NAME FROM emp_record_table
@@ -58,8 +58,9 @@ SELECT EMP_ID, FIRST_NAME, LAST_NAME FROM emp_record_table
 WHERE DEPT = "HEALTHCARE";
 
 /*
-7- Fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the 
-employee record table, and make a list of employees and details of their department.
+7- List down employee details such as EMP_ID, FIRST_NAME, LAST_NAME, ROLE, 
+DEPARTMENT, and EMP_RATING grouped by dept. Also include the respective employee rating along 
+with the max emp rating for the department.
     */
 
 SELECT e.EMP_ID, e.FIRST_NAME, e.LAST_NAME, e.ROLE, e.DEPT, e.EMP_RATING, d.max_emp_rating
@@ -71,8 +72,8 @@ JOIN (
 ) d ON e.DEPT = d.DEPT;
 
 /*
-8- Fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the 
-employee record table, and make a list of employees and details of their department.
+8- Calculate the minimum and the maximum salary of the employees in each role. Take 
+data from the employee record table.
     */
 
 SELECT MIN(SALARY) AS MIN_SALARY, MAX(SALARY) AS MAX_SALARY, ROLE
@@ -80,16 +81,16 @@ FROM emp_record_table
 GROUP BY ROLE;
 
 /*
-9- Fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the 
-employee record table, and make a list of employees and details of their department.
+9- Assign ranks to each employee based on their experience. Take data from the 
+employee record table.
     */
 
 SELECT EMP_ID, FIRST_NAME, LAST_NAME, RANK() OVER (ORDER BY EXP DESC) AS ExpRank
 from emp_record_table;
 
 /*
-10- Fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the 
-employee record table, and make a list of employees and details of their department.
+10- Create a view that displays employees in various countries whose salary is more 
+than six thousand. Take data from the employee record table.
     */
 
 CREATE VIEW EM AS 
@@ -100,16 +101,16 @@ WHERE SALARY > 6000;
 SELECT * FROM EM;
 
 /*
-11- Fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the 
-employee record table, and make a list of employees and details of their department.
+11- Find employees with experience of more than ten years. Take data from the 
+employee record table.
     */
 
 SELECT EMP_ID, FIRST_NAME, LAST_NAME, EXP
 FROM emp_record_table WHERE EMP_ID IN (SELECT EMP_ID FROM emp_record_table WHERE EXP > 10);
 
 /*
-12- Fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the 
-employee record table, and make a list of employees and details of their department.
+12- Create a stored procedure to retrieve the details of the employees whose experience 
+is more than three years. Take data from the employee record table.
     */
 
 DELIMITER &&
@@ -121,8 +122,14 @@ CALL get_experience_details();
 DELIMITER;
 
 /*
-13- Fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the 
-employee record table, and make a list of employees and details of their department.
+13- check whether the job profile assigned to 
+each employee in the data science team matches the organization’s set standard.
+The standard being:
+For an employee with experience less than or equal to 2 years assign 'JUNIOR DATA SCIENTIST',
+For an employee with the experience of 2 to 5 years assign 'ASSOCIATE DATA SCIENTIST',
+For an employee with the experience of 5 to 10 years assign 'SENIOR DATA SCIENTIST',
+For an employee with the experience of 10 to 12 years assign 'LEAD DATA SCIENTIST',
+For an employee with the experience of 12 to 16 years assign 'MANAGER'
     */
 
 DELIMITER &&
@@ -148,8 +155,8 @@ SELECT EMP_ID, FIRST_NAME, LAST_NAME, EXP, Role,
 CheckJobRoel(EXP) AS Assigned_Job_Profile FROM data_science_team;
 
 /*
-14- Fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the 
-employee record table, and make a list of employees and details of their department.
+14- Find the employee whose 
+FIRST_NAME is ‘Eric’ in the employee table after checking the execution plan.
     */
 
 CREATE INDEX FNameCheck
@@ -159,8 +166,8 @@ SELECT FIRST_NAME FROM emp_record_table
 WHERE (FIRST_NAME = "Eric"); 
 
 /*
-15- Fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the 
-employee record table, and make a list of employees and details of their department.
+15- Calculate the bonus for all the employees, based on their ratings and salaries (Use 
+the formula: 5% of salary * employee rating).
     */
 
 SELECT EMP_ID, FIRST_NAME, LAST_NAME, SALARY, EMP_RATING,
@@ -168,8 +175,8 @@ SELECT EMP_ID, FIRST_NAME, LAST_NAME, SALARY, EMP_RATING,
 FROM emp_record_table;
 
 /*
-16- Fetch EMP_ID, FIRST_NAME, LAST_NAME, GENDER, and DEPARTMENT from the 
-employee record table, and make a list of employees and details of their department.
+16- Calculate the average salary distribution based on the continent and country. Take 
+data from the employee record table.
     */
 
 SELECT CONTINENT, COUNTRY, AVG(SALARY)
